e920f19529add2b3fdc3e086c30356feca337a16
Fujimura Daisuke <me@fujimuradaisuke.com>
2013-04-20 16:35:03 +0900

    Remove tabs

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -12,24 +12,24 @@ spec :: Spec
 spec = do
     describe "GET /" $ do
       it "should contain 'Hello' in response body" $ do
-	app <- liftIO $ Scotty.scottyApp App.app
-	body <- WT.simpleBody <$> app `get` ""
-	body `shouldSatisfy` \x -> any (LBS.isPrefixOf "Happy Holidays") $ LBS.tails x
+        app <- liftIO $ Scotty.scottyApp App.app
+        body <- WT.simpleBody <$> app `get` ""
+        body `shouldSatisfy` \x -> any (LBS.isPrefixOf "Happy Holidays") $ LBS.tails x
 
     describe "GET /" $ do
       it "should contain 'Guten tag' in response body" $ do
-	app <- getApp
-	body <- getBody <$> app `get` ""
-	body `shouldContain` "Guten tag"
+        app <- getApp
+        body <- getBody <$> app `get` ""
+        body `shouldContain` "Guten tag"
 
     describe "GET /bar" $ do
       it "should redirect to /foo" $ do
-	app <- getApp
-	res <- app `get` "bar"
-	res `shouldRedirectTo` "/foo"
+        app <- getApp
+        res <- app `get` "bar"
+        res `shouldRedirectTo` "/foo"
 
     describe "GET /foobar" $ do
       it "should respond with 404" $ do
-	app <- getApp
-	res <- app `get` "foobar"
-	res `shouldRespondWith` 404
+        app <- getApp
+        res <- app `get` "foobar"
+        res `shouldRespondWith` 404
