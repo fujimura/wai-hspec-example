564cf4bff438428fe126431090b112bdb879a8b4
Simon Hengel <sol@typeful.net>
2014-06-01 21:37:46 +0800

    Initial version of hspec-wai

--- src/App.hs
+++ src/App.hs
@@ -4,15 +4,7 @@ module App (app) where
 import           Network.Wai
 import           Web.Scotty hiding (body)
 
-import           Views
-
 app :: IO Application
 app = scottyApp $ do
-    get "/" $
-        mustache "src/Views/index.mustache" $ indexView "Happy Holidays" "from Fujimura"
-
-    get "/foo" $
-        mustache "src/Views/foo.mustache" $ fooView "Foo" "Bar"
-
-    get "/bar" $
-        redirect "/foo"
+  get "/foo" $
+    text "bar"
